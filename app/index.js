5d4376e68412c12c0626e8ec6f21d08be57a89c9
bin lu <lu.bin.lu.bin@gmail.com>
2017-05-19 15:54:12 -0400

    basics

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-//import css from './index.css';
-import App from './components/app';
+let store = createStore(todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-ReactDOM.render(
-  <App />,
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
-);
+)
